9a7319402d7dd3d8bf58859b6f8b6dc38673305d
the-fool <sketchbang@gmail.com>
2016-01-10 21:02:57 -0600

    more getting files in order

--- app/frontend/main/views.py
+++ app/frontend/main/views.py
@@ -1,7 +1,8 @@
 from . import main
 from flask import render_template
-#from app.models import *
+from app.models import Tire
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    tires = Tire.query.all()
+    return render_template('index.html', tires=tires)
