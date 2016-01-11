8f89bfa249ef5073c37eae06edf4dd017bf50d11
the-fool <sketchbang@gmail.com>
2016-01-11 09:43:48 -0600

    add decimal formatter

---
+++ app/frontend/static/js/index.js
@@ -0,0 +1,29 @@
+
+$('a.modal-anchor').click(function () {
+    var id = $(this).data('id');
+    var $m = $('#tire-modal div.modal-body');
+    $m.hide();
+    $.ajax({
+            url: '/api/products',
+            data: {
+                id: id
+            }
+        })
+        .done(function (data) {
+          $m.show();
+          $m.find('#name').text(data.name);
+          $m.find('img').attr('src', '/static/img/'+data.image);
+          $m.find('#description').text(data.description);
+          $m.find('#price').text('$'+data.price.toFixed(2));
+        });
+});
+
+$(function() {
+    $('div.portfolio-item div.itemdesc h4.t-price').each(function() {
+        $(this).text(function(i,t) {
+            console.log(i);
+            console.log(t);
+           return '$' + parseFloat(t).toFixed(2); 
+        });
+    });
+});
