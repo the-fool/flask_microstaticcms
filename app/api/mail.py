89ef42f8ef0d95462e9328dba65dda39a8eeb0a0
the-fool <sketchbang@gmail.com>
2016-01-12 09:13:05 -0600

    implement email support

---
+++ app/api/mail.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request, Response
+from flask.ext.mail import Message
+
+from app.factory import mail
+
+bp = Blueprint('mail', __name__, url_prefix='/mail')
+
+@bp.route('/contact_me', methods=['POST'])
+def contact_me():
+    d = {}
+    for x in ['name','email','phone','message','email']:
+        if not request.form[x]:
+            print "missing {} field".format(x)
+            return Response(400)
+        d[x] = request.form[x]
+        
+    subject = 'Montana Tire: ' + d['name']
+    body = 'You have a new message form MTW: \n\n'
+    body += 'Name: {}\n'.format(d['name'])
+    body += 'Email: {}\n'.format(d['email'])
+    body += 'Phone: {}\n\n'.format(d['phone'])
+    body += 'Message: {}\n'.format(d['message'])
+    
+    msg = Message(subject=subject, 
+                  body=body,
+                  sender="bob@montanatireandwheel.com", 
+                  recipients=['sketchbang@gmail.com'])
+    try:
+        mail.send(msg)
+    except:
+        return Response(503)
+    
+    return Response(200)
